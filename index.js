afac734ad6b9e6f2b784385f05208959c32bd3b9
hangwoo <marong61@gmail.com>
2021-11-08 14:50:22 +0900

    fix: remove await

--- index.js
+++ index.js
@@ -6,20 +6,23 @@ try {
   const languages = core.getInput('languages').split(',');
   const projectId = core.getInput('project_id');
   const targetProgress = core.getInput("target_progress");
-  const data = await fetch(`https://api.crowdin.com/api/v2/projects/${projectId}/languages/progress`, {
+  fetch(`https://api.crowdin.com/api/v2/projects/${projectId}/languages/progress`, {
     headers: {
       "content-type": "applications/json",
       "Authorization" : `Bearer ${token}`,
     }
-  });
-  const progress = languages.map(languageId => getProgress({ data, languageId }));
-  progress.forEach((result, index) => {
-    core.setOutput(`${languages[index]} progress`, result);
-    if (Number(result) < Number(targetProgress)) {
-      const err = new Error("Low progress");
-      err.message = `Lower than target progress in languageId:${languages[index]}`;
-      throw err;
-    }
+  }).then(({ data }) => {
+    const progress = languages.map(languageId => getProgress({ data, languageId }));
+    progress.forEach((result, index) => {
+      core.setOutput(`${languages[index]} progress`, result);
+      if (Number(result) < Number(targetProgress)) {
+        const err = new Error("Low progress");
+        err.message = `Lower than target progress in languageId:${languages[index]}`;
+        throw err;
+      }
+    });
+  }).catch(err => {
+    throw err;
   });
 } catch (error) {
   core.setFailed(error.message);
